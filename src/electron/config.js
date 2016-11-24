6ad285474c68d63279b04ba77840531e7901b7d0
nilar <nicolas.larche@rca.fr>
2016-11-24 16:54:06 +0100

    Default screen size

--- src/electron/config.js
+++ src/electron/config.js
@@ -3,24 +3,21 @@ import path from 'path';
 
 export default () => {
   const screenSize = screen.getPrimaryDisplay().workAreaSize;
+
   const defaultHeight = (screenSize.height * 3) / 4;
   const defaultWidth = (screenSize.width * 3) / 4;
 
-
   const baseConfig = {
-    width:  defaultWidth || 800,
-    height: defaultHeight || 600,
-    minWidth: 200,
-    minHeight: 200,
+    width: parseInt(defaultWidth) || 800,
+    height: parseInt(defaultHeight) || 600,
+    center: true,
     show: true,
     autoHideMenuBar: true,
     icon: path.resolve(__dirname + '/../assets/icon.png'),
     title: 'Qobuz Desktop player',
     webPreferences: {
       nodeIntegration: true,
-      //preload: path.resolve(`${__dirname}/../renderer/generic/index.js`),
     },
   };
-
   return baseConfig;
 };
