6b4099bd751a380228d27943aded75b9124d1fee
nilar <nicolas.larche@rca.fr>
2016-11-24 16:00:02 +0100

    Readme

--- front/preload.js
+++ front/preload.js
@@ -1,11 +1,6 @@
 console.log('preload.js')
 
-/*
-player - play - button
-player - previous - button
-player - next - button
 
-*/
 const { ipcRenderer } = require('electron');
 
 ipcRenderer.on('player:action', function(event, action) {
