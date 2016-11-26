959d2bc530c9d14800ff46c69d6f3f49313adc0f
nlarche <nlarche@hotmail.fr>
2016-11-26 14:51:38 +0100

    add app icon

--- src/electron/config.js
+++ src/electron/config.js
@@ -13,7 +13,7 @@ export default () => {
     center: true,
     show: true,
     autoHideMenuBar: true,
-    icon: path.resolve(__dirname + '/../assets/icon.png'),
+    icon: path.resolve(__dirname + '/../assets/24x24.png'),
     title: 'Qobuz Desktop player',
     webPreferences: {
       nodeIntegration: true,
